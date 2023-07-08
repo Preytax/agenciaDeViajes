6e32cc8a0aa8a4af12e349f73d122401d44047e5
FabyEspinoza5 <faesya838@gmail.com>
2023-07-08 11:34:42 -0500

    comentarios-Fabian

---
+++ back/src/main/java/com/agencia_viaje/back/model/mdl_Comentarios.java
@@ -0,0 +1,30 @@
+package com.agencia_viaje.back.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name ="agv_comentarios")
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class mdl_Comentarios {
+    @Id
+    @Column(name="ID")
+    @Getter @Setter
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name="MENSAJE")
+    @Getter @Setter
+    private String mensaje;
+}
---
+++ front/src/views/comentarios/comentarios.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1>Experiencias de otros usuarios:</h1>
+        <form>
+            <input type="text" placeholder="Nombre" required>
+            <textarea placeholder="Comentario" required></textarea>
+            <button tyoe="submit">Enviar</button>
+        </form>
+        <div>
+            <p>No hay comentarios.</p>
+        </div>
+
+        <div>
+            <div>
+                <!--<h3>{{ comentarios.nombre }}</h3>
+                <p>{{ comentarios.mensaje }}</p>-->
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            id: 0,
+            comentario: '',
+            mensaje: [],
+        };
+    },
+    mounted() {
+        this.fetchComments();
+    },
+    methods: {
+        submitComment(){
+            const newComment = {
+                name: this.name,
+                Comment: this.comment,
+            };
+        }
+    }
+}
+</script>
